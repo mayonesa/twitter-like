9dc62bf4025d4728a11c9cecc4c3c67c2e3615b9
John Jimenez <mayonesa.cosmica@gmail.com>
2014-01-21 15:03:34 -0500

    asynchronous calls of methods that return void (post and follows) to improve performance

--- src/main/groovy/johnjimenez/usbinterview/twitterlike/interpreter/FollowExpression.groovy
+++ src/main/groovy/johnjimenez/usbinterview/twitterlike/interpreter/FollowExpression.groovy
@@ -1,13 +1,15 @@
 package johnjimenez.usbinterview.twitterlike.interpreter
 
 import javax.inject.*
+import org.springframework.scheduling.annotation.Async
 
 @Named
 class FollowExpression {
     @Inject
     def service
     
-    def interpret(followerName, followeeName) {
+    @Async
+    void interpret(followerName, followeeName) {
         service.follow followerName, followeeName
     }
 }
--- src/main/groovy/johnjimenez/usbinterview/twitterlike/interpreter/PostExpression.groovy
+++ src/main/groovy/johnjimenez/usbinterview/twitterlike/interpreter/PostExpression.groovy
@@ -1,13 +1,15 @@
 package johnjimenez.usbinterview.twitterlike.interpreter
 
 import javax.inject.*
+import org.springframework.scheduling.annotation.Async
 
 @Named
 class PostExpression {
     @Inject
     def service
     
-    def interpret(message, posterName) {
+    @Async
+    void interpret(message, posterName) {
         service.post message, posterName
     }
 }
