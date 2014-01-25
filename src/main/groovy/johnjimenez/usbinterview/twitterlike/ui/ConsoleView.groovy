6a4e64536bca0dd4c52b907da040b32095f6b919
John Jimenez <mayonesa.cosmica@gmail.com>
2014-01-25 12:31:23 -0500

    help and friendlier wait times

--- src/main/groovy/johnjimenez/usbinterview/twitterlike/ui/ConsoleView.groovy
+++ src/main/groovy/johnjimenez/usbinterview/twitterlike/ui/ConsoleView.groovy
@@ -4,14 +4,24 @@ import org.springframework.context.support.ClassPathXmlApplicationContext
 
 class ConsoleView {
     static void main(String[] args) {
-        print 'Welcome to TwitterLike.\n> '
+        def HELP = '''• posting (and the only way to create a user): <user name> -> <message>
+• reading: <user name>
+• following: <user name> follows <another user>
+• wall: <user name> wall'''
+        def HELP_COMMAND = '-h'
         def output
         def evaluator = new ClassPathXmlApplicationContext("applicationContext.xml")
             .getBean('evaluator')
+
+        print "\nWelcome to TwitterLike (\'$HELP_COMMAND\' for help).\n> "
         System.in.eachLine { command ->
-            output = evaluator.interpret(command)
-            if (output != null) {
-                println output
+            if (command == HELP_COMMAND) {
+                println HELP
+            } else {
+                output = evaluator.interpret(command)
+                if (output != null) {
+                    println output
+                }
             }
             print '> '
         }
